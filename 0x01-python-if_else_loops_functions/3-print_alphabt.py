21c625714e8ee5a826cc96ea6ed0ed70adcc2cad
tamermaarouf <tamermaarouf@gmail.com>
2024-08-29 21:50:19 -0700

    alpha letter

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alpha_letter in range(97, 123):
-    if(alpha_letter != 101) and (alpha_letter != 113):
+    if (alpha_letter != 101) and (alpha_letter != 113):
         print("{:c}".format(alpha_letter), end='')
