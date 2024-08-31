51abc5ca56c2d02982f2ae2b64af91bc208cd214
tamermaarouf <tamermaarouf@gmail.com>
2024-08-31 00:50:24 -0700

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,  100):
     if i == 99:
-        print(f"{i}")
+        print("{0}".format(i))
     else:
-        print(f'{"0" if i < 10 else ""}{i}', end=', ')
+        print('{}{}'.format("0" if i < 10 else "", i), end=', ')
