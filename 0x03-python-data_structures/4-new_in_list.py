06af6ec077cc74aa1cb266fa753ae284805b55fa
tamermaarouf <tamermaarouf@gmail.com>
2024-09-05 13:28:31 -0700

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if idx < 0 or idx > (len(my_list)):
+        return new_list
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                new_list[i] = element
+                return new_list
