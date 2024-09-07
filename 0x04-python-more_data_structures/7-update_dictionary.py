21d7773fcc59e9b6098773f114d0f675e0b32b47
tamermaarouf <tamermaarouf@gmail.com>
2024-09-07 07:53:49 -0700

    7. Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Write a function that replaces or adds key/value in a dictionary.
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return (a_dictionary)
