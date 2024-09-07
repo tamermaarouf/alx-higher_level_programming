9ae317ffe5bee682c4c2795895a7d217db38828d
tamermaarouf <tamermaarouf@gmail.com>
2024-09-07 03:02:48 -0700

    Search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    '''
-    new_matrix = []
-    for i in range(len(matrix)):
-        mul_matrix = []
-        for x in range(len(matrix[i])):
-            mul_matrix.append(matrix[i][x] * matrix[i][x])
-        new_matrix.append(mul_matrix)
-    '''
     return ([[x**2 for x in i] for i in matrix])
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    '''
     new_list = []
     for num in range(len(my_list)):
         if my_list[num] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[num])
-    return (new_list)
+            '''
+    return ([replace if number == search else number for number in my_list])
