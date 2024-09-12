680c81b69fa380ccee80c043f66cbe0275b12788
tamermaarouf <tamermaarouf@gmail.com>
2024-09-11 21:01:43 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Write a class Square that defines '''
+
+
+class Square():
+    '''
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+    Attributes:
+        size (int): The size of a square is crucial for a square,
+    '''
+    def __init__(self. size):
+        self.__size = size
