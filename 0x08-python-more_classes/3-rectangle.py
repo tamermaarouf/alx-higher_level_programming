c482d97147ae4ed457ab4b83a3740f10470abc36
tamermaarouf <tamermaarouf@gmail.com>
2024-09-14 13:42:37 -0700

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,5 +53,4 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ''
-        else:
-            return '\n'.join('#' * self.__width for col in range(self.__height))
+        return '\n'.join('#' * self.__width for _ in range(self.__height))
