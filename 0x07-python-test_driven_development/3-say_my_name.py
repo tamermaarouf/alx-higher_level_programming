cd27254c2d6ce1fa06a2da1f38d1e4a5bb6d638a
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-24 20:12:22 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-''' Write a function that prints My name is <first name> <last name> '''
 
 
 def say_my_name(first_name, last_name=""):
-    '''first_name and last_name must be strings otherwise,
-    raise a TypeError exception with the message first name must be a string'''
-
+    '''This function prints name (<first name> <last name>)
+    Args:
+    first_name (str): The fisrt name to be printed
+    last_name (str): The last name to be printed
+    Raises:
+    TypeError: If either the first_name and last_name are not strings'''
     if not isinstance(first_name, str):
         raise TypeError("first name must be a string")
     if not isinstance(last_name, str):
