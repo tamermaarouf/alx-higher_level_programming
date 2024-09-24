2878819a5efc273fc8ace72f35c428a7eec73712
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-24 20:57:26 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,3 +10,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
