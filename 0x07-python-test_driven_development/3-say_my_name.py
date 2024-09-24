a5170f6d5291ce067487b719275742da8f7ce8a9
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-24 18:02:46 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
-    if (not isinstance(first_name, str)):
+    if (not isinstance(first_name, str)) or (not first_name):
         raise TypeError("first name must be a string")
     if (not isinstance(last_name, str)):
         raise TypeError("last name must be a string")
