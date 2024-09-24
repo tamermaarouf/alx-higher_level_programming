ee6c098ccc0beadf5d26aff4ab0f64670aa9e14b
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-24 17:41:58 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,14 @@
 
 
 def say_my_name(first_name, last_name=""):
-    if (not first_name):
+    '''if (not first_name):
         raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
+    '''
     if (not (isinstance(first_name, str))):
         raise TypeError("first name must be a string")
-    if not (first_name.strip()):
+    '''if not (first_name.strip()):
         raise TypeError("first name must be a string")
+    '''
     if (not (isinstance(last_name, str))):
         raise TypeError("last name must be a string")
     else:
