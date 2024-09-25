4cedc2f5d8184fe3466138a32d906b4a614217c3
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-26 01:36:55 +0300

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Write a class MyList that inherits from list:'''
+
+class MyList(list):
+    def __init__(self):
+        self.__new_list = []
+    '''def append(self, item):
+        super().append(self._validate_number(item))'''
+    def print_sorted(self):
+        self.__new_list = self.copy()
+        self.__new_list.sort()
+        print(self.__new_list)
+
+    def _validate_number(self, value):
+        if isinstance(value, (int, float, complex)):
+            return value
+        raise TypeError(
+            f"numeric value expected, got {type(value).__name__}"
+        )
