4a1c10a1df8e8f7fafdb307f842270a75794502f
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-26 01:50:24 +0300

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,15 +5,17 @@
 class MyList(list):
     def __init__(self):
         self.__new_list = []
-    '''def append(self, item):
-        super().append(self._validate_number(item))'''
+    
+    def append(self, item):
+        super().append(self._validate_number(item))
+
     def print_sorted(self):
         self.__new_list = self.copy()
         self.__new_list.sort()
         print(self.__new_list)
 
     def _validate_number(self, value):
-        if isinstance(value, (int, float, complex)):
+        if isinstance(value, int):
             return value
         raise TypeError(
             f"numeric value expected, got {type(value).__name__}"
