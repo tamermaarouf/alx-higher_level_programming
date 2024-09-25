786a72ff9f02eec7c150f9aca5cce784ca3ebd42
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-26 02:51:14 +0300

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,20 +3,9 @@
 
 
 class MyList(list):
-    def __init__(self):
-        self.__new_list = []
-    
-    def append(self, item):
-        super().append(self._validate_number(item))
-
+    '''
+    Public instance method: that prints the list, but sorted (ascending sort)
+    You can assume that all the elements of the list will be of type int
+    '''
     def print_sorted(self):
-        self.__new_list = super().copy()
-        self.__new_list.sort()
-        print(self.__new_list)
-
-    def _validate_number(self, value):
-        if isinstance(value, int):
-            return value
-        raise TypeError(
-            f"numeric value expected, got {type(value).__name__}"
-        )
+        print(sorted(self))
