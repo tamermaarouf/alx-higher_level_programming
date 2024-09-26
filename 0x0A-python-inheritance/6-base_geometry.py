e8b5ce01b3a1df0c5c0b75a7c0d5410e13a26a20
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-26 04:29:39 +0300

    6. Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,5 +5,5 @@
 class BaseGeometry:
     '''A BaseGeometry class.'''
     def area(self):
-        '''that raises an Exception with the message area() is not implemented'''
+        '''Exception with the message area() is not implemented'''
         raise Exception("area() is not implemented")
