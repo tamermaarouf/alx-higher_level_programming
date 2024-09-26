1ed1e4d31d626e1feac467a40a974d295c2e977d
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-26 03:35:55 +0300

    3. Same class or inherit from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''Determines if an object is exactly an instance of a class.'''
-    return type(obj) is a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
