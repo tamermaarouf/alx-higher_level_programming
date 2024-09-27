1b36f84b6c4b5046c39329aece72012f300b132f
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-27 11:18:32 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Write a function that reads a text file (UTF8) and prints it to stdout:
+'''Write a function that reads a text file (UTF8) and prints it to stdout:'''
 
 
 def read_file(filename=""):
+    '''You must use the with statement'''
     with open(filename, encoding="utf-8") as r_file:
         for line in r_file:
             print(line, end='')
