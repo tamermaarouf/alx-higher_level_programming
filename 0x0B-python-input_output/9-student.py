f8c2ffa6a8555410592fc832d9f8aa340180072e
tamer_maarouf <tamermaarouf@gmail.com>
2024-09-29 00:16:52 +0300

    9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,7 @@ class Student:
     '''Instantiation with first_name, last_name and age'''
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.last = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
