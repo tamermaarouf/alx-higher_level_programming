81f6c92c2b2825a814fcb0d53d2484b2079c96ea
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-07 14:23:38 +0300

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+// print process.argv
+let total = 0;
+argv.forEach((index) => {
+  total++;
+});
+if (total === 3) {
+  console.log('Argument found');
+} else if (total > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
