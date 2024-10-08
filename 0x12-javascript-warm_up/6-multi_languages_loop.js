872d72868056971e8003f344825d778c7409fc37
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-09 01:01:34 +0300

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const word of myArray) { console.log(word); }
