206f12438511a42085a4d4fcb796ab0596317718
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-09 02:15:50 +0300

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,10 +5,13 @@ const { argv } = require('node:process');
 let count = 0;
 let message = 'Missing number of occurrences';
 argv.forEach((val, index) => {
-  if ((index === 2) && (!isNaN(val))) { count = parseInt(val); message = 'C is fun'; }
+  if ((index === 2) && (!isNaN(val))) { count = parseInt(val); }
 });
 if (count > 0) {
+  message = 'C is fun';
   for (let i = 0; i < count; i++) {
     console.log(message);
   }
+} else if (count < 0) {
+  process.exit(0);
 } else { console.log(message); }
