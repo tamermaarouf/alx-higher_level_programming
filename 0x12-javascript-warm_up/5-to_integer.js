5cdd8211349dcacca4a9f9325519a806ae95d127
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-09 00:50:20 +0300

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@
 const { argv } = require('node:process');
 let num = 'Not a number';
 argv.forEach((val, index) => {
-  if ((index === 2) && (!isNaN(val))) { num = parseInt(val); }
+  if ((index === 2) && (!isNaN(val))) { num = `My number: parseInt(${val})`; }
 });
-const message = `My number: ${num}`;
+const message = num;
 console.log(message);
