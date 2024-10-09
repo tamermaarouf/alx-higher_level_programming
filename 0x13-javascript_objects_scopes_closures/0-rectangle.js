687363959fea8f4bbad5fcaa46c16920c7b1b12b
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-09 08:29:35 +0300

    1. Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle:
+
+// eslint-disable-next-line no-unused-vars, no-shadow
+class Rectangle {
+
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write an empty class Rectangle that defines a rectangle:
+
+// eslint-disable-next-line no-unused-vars, no-shadow
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
