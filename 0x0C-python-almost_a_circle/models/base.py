ca66f27a7f26cfdf95410a40ac227895a4b275c8
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-10 15:58:25 +0200

    1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+'''
+Write the first class Base:
+'''
+
+
 class Base():
     __nb_objects = 0
+
     def __init__(self, id=None):
+        '''
+        if id is not None, assign the public instance attribute id with
+        this argument value - you can assume id is an integer and
+        you don’t need to test the type of it
+        otherwise, increment __nb_objects and assign
+        the new value to the public instance attribute id
+        '''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id =Base.__nb_objects
+            self.id = Base.__nb_objects
