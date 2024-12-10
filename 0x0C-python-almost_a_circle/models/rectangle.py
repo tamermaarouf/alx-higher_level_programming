11dce097e026be98a9f41aa63fa93f40d01d442d
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-10 17:21:53 +0200

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     '''
     def __init__(self, width, height, x=0, y=0, id=None):
         '''constractor'''
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):
