84a4bd5b700ab1a9948e8f257ce20b4978c38f10
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-10 17:20:45 +0200

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,6 @@ class Rectangle(Base):
     Class Rectangle inherits from Base
     Private instance attributes, each with its own public getter and setter:
     '''
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
-
     def __init__(self, width, height, x=0, y=0, id=None):
         '''constractor'''
         self.__width = width
