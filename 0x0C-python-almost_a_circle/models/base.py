0a777f8c40373d1317fcecc52f4910d1e6577750
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-13 10:15:46 +0200

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,5 +27,4 @@ class Base():
         '''returns the JSON string representation of list_dictionaries:'''
         if list_dictionaries is None or list_dictionaries == '[]':
             return '[]'
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
